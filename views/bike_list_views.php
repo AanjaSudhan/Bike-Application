9da9055fdde26c4aa48edb3c3b0553e9f858a6eb
AanjaSudhan <aanjasudhan@gmail.com>
2018-02-19 21:00:35 +0530

    remove tag

--- views/bike_list_views.php
+++ views/bike_list_views.php
@@ -1,11 +1,10 @@
 
-<link href="../theme.css" rel="stylesheet">
 
 <ul>
 	
 <?php foreach ($bikelist as $index => $bike) { ?>
 <?php echo "<li>" ?>
-<a  href= "index.php?id=<?php echo $bike["id"]?>"><?php echo $bike["name"];?></a>
+<a class="link" href= "index.php?id=<?php echo $bike["id"]?>"><?php echo $bike["name"];?></a>
 <a href= "index.php?deleteid=<?php echo $bike["id"]?>"> &#128465</a>
 <a href= "views/addbike.php?editid=<?php echo $bike["id"]?>"> &#9997</a><br>
 <?php echo "</li>" ?>
@@ -14,3 +13,41 @@
 
 
 </ul>
+
+
+
+
+
+
+
+<style>
+
+a{
+
+	text-decoration: none;
+	text-transform: uppercase;
+	margin-left: 10px;
+	color:rgb(142, 255, 170);
+	
+
+}
+
+.link{
+	background:rgb(162, 63, 255);
+	color:rgb(2, 239, 112);
+	font-style:italic;
+	padding: 2px;
+	padding-left: 2px;
+	border-radius: 5px;
+
+}
+.link:hover{
+	background:rgb(242, 75, 91);
+	color:black;
+
+}
+
+li{
+	list-style-type: none;
+}
+</style>
